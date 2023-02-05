39586acf57df5636359fb892b81a1510141ab773
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-02-06 02:30:49 +0800

    update: github link

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
     return [
       {
         source: "/github",
-        destination: "https://github.com/llegomark/affirmations",
+        destination: "https://github.com/llegomark/betteremails",
         permanent: false,
       },
       {
